94755d22943c761218548dfa02fd5be248888dac
markpratt <prattmk2010@gmail.com>
2015-10-24 02:43:18 -0700

    Revert "Commit to be reverted"
    This reverts commit 5e97a2fc18add39a9ab49c6a9480e945c8e29f01.

--- Meetup/app/src/main/java/jefferyvicente/meetup/EventDetails.java
+++ Meetup/app/src/main/java/jefferyvicente/meetup/EventDetails.java
@@ -8,24 +8,14 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-import com.parse.ParseUser;
-import com.parse.FindCallback;
-import com.parse.GetCallback;
-import com.parse.ParseException;
-import java.util.List;
-
 public class EventDetails extends Activity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
-        getActionBar().hide();
 
-        // Get extras from eventView activity
+        // Get the data passed from eventView and populate this activity
         Bundle myInput = this.getIntent().getExtras();
         if(myInput == null)
             Log.d("debug", "Intent was null");
@@ -33,36 +23,27 @@ public class EventDetails extends Activity
         {
             Log.d("debug", "Intent was ok");
 
-            final TextView ntv = (TextView) this.findViewById(R.id.name_TextView);
-            final TextView dtv = (TextView) this.findViewById(R.id.details_TextView);
-            final TextView ltv = (TextView) this.findViewById(R.id.location_TextView);
-            final TextView datv = (TextView) this.findViewById(R.id.date_TextView);
-            final TextView ttv = (TextView) this.findViewById(R.id.time_TextView);
+            TextView ntv = (TextView) this.findViewById(R.id.name_TextView);
+            String eventName = myInput.getString("selectedName");
+            ntv.setText(eventName);
 
-            // Assume eventName is unique. Perform query to get other data for event and then set TextViews.
-            ParseQuery<ParseObject> query = ParseQuery.getQuery("event");
-            query.whereEqualTo("eventName", myInput.getString("selectedName"));
-            query.getFirstInBackground(new GetCallback<ParseObject>() {
-                public void done(ParseObject object, ParseException e) {
-                    if (object == null) {
-                        Log.d("event", "The getFirst request failed.");
-                    } else {
-                        Log.d("event", "Retrieved the object.");
+            TextView dtv = (TextView) this.findViewById(R.id.details_TextView);
+            String eventDetails = myInput.getString("selectedDetail");
+            dtv.setText(eventDetails);
 
-                        String eventName = object.getString("eventName");
-                        String eventDate = object.getString("eventDate");
-                        String eventTime = object.getString("eventTime");
-                        String eventDetails = object.getString("eventDetails");
-                        String eventLocationAddress = object.getString("eventLocationAddress");
+            TextView ltv = (TextView) this.findViewById(R.id.location_TextView);
+            String eventLocation = myInput.getString("selectedAddress");
+            ltv.setText(eventLocation);
 
-                        ntv.setText(eventName);
-                        datv.setText(eventDate);
-                        ttv.setText(eventTime);
-                        dtv.setText(eventDetails);
-                        ltv.setText(eventLocationAddress);
-                    }
-                }
-            });
+            TextView datv = (TextView) this.findViewById(R.id.date_TextView);
+            String eventDate = myInput.getString("selectedDate");
+            datv.setText(eventDate);
+
+            TextView ttv = (TextView) this.findViewById(R.id.time_TextView);
+            String eventTime = myInput.getString("selectedTime");
+            ttv.setText(eventTime);
+
+
 
         }
 
